478a147ce5a510363882d40524f36f91dac7191c
HugoSky <win0032@163.com>
2023-06-28 15:59:43 +0800

    feat: 去掉 mock

--- packages/server/src/utils/RPA.ts
+++ packages/server/src/utils/RPA.ts
@@ -114,22 +114,22 @@ export const buildTool = (manifest: IManifest) => {
                 async _call(input: string) {
                     try {
                         // mock
-                        const cardId = '16db934a-dc09-4e51-8725-88a38e206916.schema';
-                        return JSON.stringify({
-                            type: 'card',
-                            cardId,
-                            cardData: {
-                                script_url: 'https://code.alibaba-inc.com/tianqi.ctq/public-pages/blob/master/rpa_coffee.js',
-                                app_url: 'dingtalk://platformapi/startapp?appId=2021001108668186&mini_app_launch_scene=op_thzy',
-                                home_url: 'dingtalk://platformapi/startapp?appId=2021001108668186&mini_app_launch_scene=op_thzy',
-                                inputs: {
-                                    name: '桂花琉璃',
-                                    cup: '超超超大杯',
-                                    temp: '正常冰',
-                                    sugar: '不另外加糖'
-                                }
-                            }
-                        })
+                        // const cardId = '16db934a-dc09-4e51-8725-88a38e206916.schema';
+                        // return JSON.stringify({
+                        //     type: 'card',
+                        //     cardId,
+                        //     cardData: {
+                        //         script_url: 'https://code.alibaba-inc.com/tianqi.ctq/public-pages/blob/master/rpa_coffee.js',
+                        //         app_url: 'dingtalk://platformapi/startapp?appId=2021001108668186&mini_app_launch_scene=op_thzy',
+                        //         home_url: 'dingtalk://platformapi/startapp?appId=2021001108668186&mini_app_launch_scene=op_thzy',
+                        //         inputs: {
+                        //             name: '桂花琉璃',
+                        //             cup: '超超超大杯',
+                        //             temp: '正常冰',
+                        //             sugar: '不另外加糖'
+                        //         }
+                        //     }
+                        // })
 
                         if (script_url && this.cardJson) {
                             const inputs = JSON.parse(input)
