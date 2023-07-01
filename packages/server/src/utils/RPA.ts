151beca6c1b199f7c2f483e5af0169245df6ac47
HugoSky <win0032@163.com>
2023-07-02 01:10:29 +0800

    feat: prompt 调优

--- packages/server/src/utils/RPA.ts
+++ packages/server/src/utils/RPA.ts
@@ -37,11 +37,11 @@ export const buildTool = (manifest: IManifest) => {
 
                 descriptionForModel = ability?.ability_for_model?.description +
                 `你需要做以下事情：` +
-                `\n第一件事：请你根据以下参数描述定义一个 object：\n` + 
+                `\n第一件事：请你根据以下参数描述定义一个名为BIG的javascript中的对象：\n` + 
                 inputStr + `\n` +
-                `\n第二件事：如果可以从 user's input 内容中解析到 object 中参数所对应的内容，则将内容替换为 user's input 中的内容。` +
-                `\n第三件事：请你检查 object 中的每个参数值，必须确保每个参数的值不是""，` + emptyStr +
-                '\n最后将object通过JSON.stringify处理后，作为 input 返回。'
+                `\n第二件事：如果可以从 user's input 内容中解析到BIG中参数所对应的内容，则将内容替换为 user's input 中的内容。` +
+                `\n第三件事：请你检查BIG中的每个参数值，必须确保每个参数的值不是""，` + emptyStr +
+                '\n最后将BIG通过JSON.stringify处理后，作为 input 返回。'
             })
         }
         /**
